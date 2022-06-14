eb77190bf6e6866366c8027885ec007c13da2797
irrationnelle <drakkarverenis@gmail.com>
2022-06-15 04:06:12 +0900

    test: Divide tests to interpolator and shortener

--- shortener.spec.ts
+++ shortener.spec.ts
@@ -1,9 +1,7 @@
 import * as shortener from './shortener';
 import * as helper from './helper';
-import * as interpolator from "./interpolator";
 
 import {findAll, findByKey, insert, removeAll} from "./__test__/db/in-memory-db";
-import {getTimestamp} from "./helper";
 
 describe('shortener: ',() => {
   beforeEach(() => {
@@ -133,75 +131,3 @@ describe('prune', () => {
   })
 })
 
-describe("interpolator: ", () => {
-  let mock: any = null;
-  beforeEach(async () => {
-    // given
-    mock = jest.spyOn(helper, "getTimestamp").mockReturnValue('1234567890123');
-
-    interpolator.init({
-      ts: getTimestamp()
-    })
-  })
-
-  afterEach(() => {
-    // teardown
-    interpolator.destroy();
-    if(!!mock){
-      mock.mockRestore();
-    }
-  })
-
-
-
-  test("URL에 {ts}가 들어가면 timestamp를 interpolation 한다.", () => {
-    // when
-    const url = "https://sample.com?ts={ts}";
-    const interpolatedUrl = interpolator.interpolate(url);
-
-    // given
-    expect(interpolatedUrl).toBe('https://sample.com?ts=1234567890123')
-  })
-
-  test("URL에 {domain} 과 {page} 가 들어가면 jobs 와 main 을 interpolation 한다.", () => {
-    // given
-    interpolator.destroy();
-    interpolator.init({
-      domain: "jobs",
-      page: "main"
-    })
-
-    // when
-    const url = "https://sample.{domain}.com/{page}";
-    const interpolatedUrl = interpolator.interpolate(url);
-
-    // given
-    expect(interpolatedUrl).toBe('https://sample.jobs.com/main')
-  })
-
-  test("query string 을 {?ts} 형태로 사용할 수 있다", () => {
-    // when
-    const url = "https://sample.com/main{?ts}";
-    const interpolatedUrl = interpolator.interpolate(url);
-
-    // then
-    expect(interpolatedUrl).toBe('https://sample.com/main?ts=1234567890123')
-  })
-
-  test("URL에 origin 과 query string 을 조합해서 interpolation 한다.", () => {
-    // given
-    interpolator.destroy();
-    interpolator.init({
-      domain: "jobs",
-      page: "main",
-      ts: getTimestamp()
-    })
-
-    // when
-    const url = "https://sample.{domain}.com/{page}{?ts}";
-    const interpolatedUrl = interpolator.interpolate(url);
-
-    // given
-    expect(interpolatedUrl).toBe('https://sample.jobs.com/main?ts=1234567890123')
-  })
-})
